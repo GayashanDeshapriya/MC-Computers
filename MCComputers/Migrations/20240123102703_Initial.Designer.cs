3feb435ea48a4e4e7ed860286350bb3f968b23d8
GayashanDeshapriya <gayashandeshapriya1922@gmail.com>
2024-01-23 16:00:48 +0530

    DB Connection Completed

--- MCComputers/Migrations/20240123102703_Initial.Designer.cs
+++ MCComputers/Migrations/20240123102703_Initial.Designer.cs
@@ -12,7 +12,7 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace MCComputers.Migrations
 {
     [DbContext(typeof(DataContext))]
-    [Migration("20240123100627_Initial")]
+    [Migration("20240123102703_Initial")]
     partial class Initial
     {
         /// <inheritdoc />
